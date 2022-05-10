5251478cf497c4ee2af8d5be7ae5baf56f8d28ec
thyghu <32553632+thyghu@users.noreply.github.com>
2022-05-10 10:22:22 -0300

    Add files via upload

--- primos.php
+++ primos.php
@@ -1,6 +1,6 @@
 <?php
 
-function verificaPrimo($numero)
+function verificaPrimo($numero) //função para desocbrir se o número é primo
 {
 		if ($numero == 1)
 		return 0;
@@ -14,22 +14,29 @@ function verificaPrimo($numero)
 
 function primo($inicio,$fim)
 {
+	//delimitando que não pode exibir os números já informados
 	$i = $inicio +1;
 	$j = $fim-1;
 	
+	//declarando array de números primos
 	$primos = array();
 	
-	for($i;$i<=$j;$i++)
+	for($i;$i<=$j;$i++) //percorrendo intervalo informado
 	{
+		
 		if(verificaPrimo($i)==1)
-		{
+		{	
+			//se o número for primo, adiciona ao array
 			array_push($primos, $i);
 		}
 	}
+	
+	//exibe resultado
 	print_r($primos);	
 	
 }	 
 
+//teste
 primo(10,29);
 
 ?>
